4c258fc3cbf5367d0b4603185c75ab49acf8c5f7
SergeyStepanovsky <98726536+SergeyStepanovsky@users.noreply.github.com>
2023-11-08 17:42:40 +0800

    fix smth

--- GuessNumber/app/src/main/java/com/example/guessnumber/GuessActivity.java
+++ GuessNumber/app/src/main/java/com/example/guessnumber/GuessActivity.java
@@ -3,11 +3,13 @@ package com.example.guessnumber;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class GuessActivity extends AppCompatActivity {
     private TextView textViewQuestion;
+    private Button buttonHigher, buttonLower, buttonCorrect;
     private int min;
     private int max;
     private int guess;
@@ -18,6 +20,9 @@ public class GuessActivity extends AppCompatActivity {
         setContentView(R.layout.activity_guess);
 
         textViewQuestion = findViewById(R.id.textViewQuestion);
+        buttonHigher = findViewById(R.id.buttonHigher);
+        buttonLower = findViewById(R.id.buttonLower);
+        buttonCorrect = findViewById(R.id.buttonCorrect);
 
         Intent intent = getIntent();
         min = intent.getIntExtra("min", 1);
@@ -25,21 +30,61 @@ public class GuessActivity extends AppCompatActivity {
         guess = (min + max) / 2;
 
         askNumber();
+
+        buttonHigher.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onHigher();
+            }
+        });
+
+        buttonLower.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onLower();
+            }
+        });
+
+        buttonCorrect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onCorrect();
+            }
+        });
     }
 
     private void askNumber() {
-        textViewQuestion.setText(String.format("Your number is %d?", guess));
+        textViewQuestion.setText(String.format("Is your number %d?", guess));
     }
 
-    public void onHigher(View view) {
+    private void onHigher() {
         min = guess + 1;
-        guess = (min + max) / 2;
-        askNumber();
+        guess = (min + max + 1) / 2; 
+        if (min >= max) {
+            guess = max;
+            onCorrect();
+        } else {
+            askNumber();
+        }
     }
 
-    public void onLower(View view) {
+    private void onLower() {
         max = guess - 1;
         guess = (min + max) / 2;
-        askNumber();
+        if (min >= max) {
+            guess = max;
+            onCorrect();
+        } else {
+            askNumber();
+        }
+    }
+
+    private void onCorrect() {
+        textViewQuestion.setText(String.format("Your number is %d! The game is over!", guess));
+        buttonHigher.setEnabled(false);
+        buttonLower.setEnabled(false);
+        buttonCorrect.setEnabled(false);
+        // Добавьте здесь код для завершения игры или начала новой.
     }
 }
+
